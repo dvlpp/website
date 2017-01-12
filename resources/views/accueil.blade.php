c3a82dca34544ac259545b5cb4cd49f84bb3d3b1
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-01-12 13:40:41 +0100

    content

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -8,7 +8,7 @@
                 <p class="h4-like">
                     Nous<span class="color">*</span> concevons depuis 2007 des sites et des
                     applications basés sur des technologies open-source, avec
-                    une approche d'artisan&nbsp;: le développement se fait sur-mesure,
+                    une approche d'artisan&nbsp&nbsp;: le développement se fait sur-mesure,
                     l’idée étant d’avoir la main sur tout.
                     <br/>Notre domaine d'expertise technique est le développement d'applications web,
                     autour plus spécifiquement de <strong>Laravel</strong> et <strong>Vue.js</strong>
@@ -35,7 +35,7 @@
     </div>
 
     <div class="container pam">
-        <h2>Quelques références récentes (2015 / 2016)</h2>
+        <h2>Quelques références récentes (2015 / 2017)</h2>
 
         <div class="grid-3-small-1">
             @foreach($projets->where("is_open_source", 0) as $projet)
@@ -60,10 +60,9 @@
         <h2>Et en ce moment...</h2>
         <div class="grid-2-1">
             <p>
-                On travaille principalement à la <strong>refonte complète de la plateforme
-                «&nbsp;Click and Collect&nbsp;», des sites et des applications mobiles</strong>
-                des magasins du groupement EK France, et au <strong>développement d'un logiciel
-                intranet / CRM pour l'Agence culturelle d'Alsace</strong>.
+                Nous travaillons notamment au <strong>développement d'un logiciel
+                intranet / CRM pour l'Agence culturelle d'Alsace</strong>
+                et à la <strong>refonte technique du site du festival Musica</strong>.
             </p>
         </div>
 
