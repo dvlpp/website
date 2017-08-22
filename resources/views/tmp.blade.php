5c1a53a099d01960d82fc9085a5106f6688008c3
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-08-22 21:10:34 +0200

    25/08 /2

--- resources/views/tmp.blade.php
+++ resources/views/tmp.blade.php
@@ -34,23 +34,42 @@
 
     <div class="container">
         <div class="grid-2-1">
-            <ul style="font-size:1.5em;">
-                <li class="mbs">
-                    Ca commence à 19h. Oui c’est tôt, mais en même temps on sera dehors, il faut vite froid, et surtout si je me couche tard après je mets 8 jours à m’en remettre. Et puis il y a une tournoi de ping pong en attente.
-                </li>
-                <li class="mbs">
+            <div style="font-size:1.5em;">
+
+                <div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/3o7bu4tuvFVWwyoR7q" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+
+                <p class="mbl mt0">
+                    Ca commence à 19h. Oui c’est tôt, mais en même temps on sera dehors, il faut vite froid, et surtout si je me couche tard après je mets 8 jours à m’en remettre. Et puis il y a un tournoi de ping pong en attente.
+                </p>
+
+                <div style="width:100%;height:0;padding-bottom:69%;position:relative;"><iframe src="https://giphy.com/embed/5Vq60COTBIUve" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+
+                <p class="mbl mt0">
                     Il y a un thème : saucisses et vin rouge. Il faut donc rapporter soit du vin rouge, soit des saucisses.
-                </li>
-                <li class="mbs">
+                    (bon ok, il y aura aussi du Spritz, sinon les filles elles viennent pas)
+                </p>
+
+                <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/lz0fR8nwQfbq" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+
+                <p class="mbl mt0">
                     On fête ma quarantième année (oui il y a une différence), mais on a pas le droit de faire de cadeau. Sinon on vient pas. C’est soit on vient, soit on fait un cadeau.
-                </li>
-                <li class="mbs">
-                    On doit dire avant demain, mercredi, 20h, si on vient. Si on vient pas, on n’a pas besoin de dire (on déteste les excuses). Et pour le dire, c’est par SMS.
-                </li>
-                <li class="mbs">
+                </p>
+
+                <div style="width:100%;height:0;padding-bottom:76%;position:relative;"><iframe src="https://giphy.com/embed/SEp6Zq6ZkzUNW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+
+                <p class="mbl mt0">
+                    On doit dire avant demain, mercredi, 20h, si on vient. Si on vient pas, on n’a pas besoin de dire (on déteste les excuses). Et pour le dire, c’est que par SMS (parce qu'on aime les règles strictes).
+                </p>
+
+                <div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/c20UV66B7zCWA" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+
+                <p class="mbl mt0">
                     C’est au 13b rue de la gare, à Schiltigheim.
-                </li>
-            </ul>
+                </p>
+                <p class="mbl mt0">
+                    (Bisous 😘)
+                </p>
+            </div>
         </div>
     </div>
 
